3b648762ff8dee4d6730d0b48b3bfef001eeddb0
Vasanth Kandadi <vasanthkandadi@gmail.com>
2024-08-24 20:29:10 -0500

    Included @Valid, BindingResult in ProjectController

--- src/main/java/com/jira_app/ppmtool/web/ProjectController.java
+++ src/main/java/com/jira_app/ppmtool/web/ProjectController.java
@@ -2,10 +2,12 @@ package com.jira_app.ppmtool.web;
 
 import com.jira_app.ppmtool.domain.Project;
 import com.jira_app.ppmtool.services.ProjectService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +24,10 @@ public class ProjectController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
-
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Invalid Project Object",HttpStatus.BAD_REQUEST);
+        }
         projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
