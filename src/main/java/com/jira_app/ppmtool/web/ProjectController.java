9056ea9e4fc58d822328ce0a35184cbd0888de4d
Vasanth Kandadi <vasanthkandadi@gmail.com>
2024-08-24 21:03:22 -0500

    Used HashMap to parse FieldErrors

--- src/main/java/com/jira_app/ppmtool/web/ProjectController.java
+++ src/main/java/com/jira_app/ppmtool/web/ProjectController.java
@@ -8,10 +8,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -26,7 +31,12 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
         if(result.hasErrors()){
-            return new ResponseEntity<String>("Invalid Project Object",HttpStatus.BAD_REQUEST);
+            Map<String,String> errorMap = new HashMap<>();
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map>(errorMap,HttpStatus.BAD_REQUEST);
         }
         projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
