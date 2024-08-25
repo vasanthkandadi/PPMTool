dd8c9aeb7c68778c0c340181c0ca2300e135c339
Vasanth Kandadi <vasanthkandadi@gmail.com>
2024-08-24 20:11:19 -0500

    Added Validation for Project

--- src/main/java/com/jira_app/ppmtool/domain/Project.java
+++ src/main/java/com/jira_app/ppmtool/domain/Project.java
@@ -1,21 +1,31 @@
 package com.jira_app.ppmtool.domain;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.util.Date;
-
 @Entity
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String projectName;
-    private String projectIdentifier;
-    private String description;
-    private Date start_date;
-    private Date end_date;
 
+    @NotBlank(message = "Project name is required")
+    private String projectName;
+    @NotBlank(message = "Project identifier is required")
+    @Size(min=4, max=5, message = "Please use 4 or 5 characters")
+    @Column(updatable = false, unique = true)
+    private String projectIdentifier;
+    @NotBlank(message = "Project description is required")
+    private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date end_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project() {
