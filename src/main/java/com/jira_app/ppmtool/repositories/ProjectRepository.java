b28109786128ab7f6a9a08ad42387cbb0994ccf8
Vasanth Kandadi <vasanthkandadi@gmail.com>
2024-08-30 11:17:45 -0500

    Test commit

--- src/main/java/com/jira_app/ppmtool/repositories/ProjectRepository.java
+++ src/main/java/com/jira_app/ppmtool/repositories/ProjectRepository.java
@@ -4,9 +4,13 @@ import com.jira_app.ppmtool.domain.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
     Optional<Project> findByProjectIdentifier(String projectID);
+
+    @Override
+    List<Project> findAll();
 }
