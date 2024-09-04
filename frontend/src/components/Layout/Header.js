fd770b6a3bfa39cdd251bae93f9559a90ff4f5d5
Vasanth Kandadi <vasanthkandadi@gmail.com>
2024-09-04 18:28:56 -0500

    Added Project Form with state management

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     return(
         <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
         <div className="container">
-            <a className="navbar-brand" href="Dashboard.html">
+            <a className="navbar-brand" href="/dashboard">
                 ProBoard
             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
